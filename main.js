fb4ca19aa68e2cd737f3c7cd34076e13fd0bfa7c
hawkeye-sol <neu_m@gmx.de>
2025-04-27 07:52:54 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-// JavaScript Inhalt
+
+async function pay(months) {
+    const amount = months === '1' ? 0.25 : 0.5;
+    const recipient = "2ymfWmDGtvqpTWhHFn2pxhDiTLpStphVFaJDnwjSMTsi";
+    try {
+        // Hier würde normalerweise Phantom Wallet Connect und Payment aufgerufen
+        alert("Simulating payment of " + amount + " SOL to " + recipient);
+        window.location.href = "info.html";
+    } catch (error) {
+        console.error(error);
+    }
+}
+    
